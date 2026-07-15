6e80ecf63d1836e8eb7cd55e5fb87fb2c218c26d
chyken-nuggets <crusadersio.ezekiel06@gmail.com>
2026-07-15 08:58:54 +0800

    refactor: add register method for two-part block + shared getConfiguredModels method

--- src/main/java/dev/chyken/data/LogicBlockStateProvider.java
+++ src/main/java/dev/chyken/data/LogicBlockStateProvider.java
@@ -3,7 +3,7 @@ package dev.chyken.data;
 import dev.chyken.block.LogicBlocks;
 import dev.chyken.block.LogicGateBlock;
 import dev.chyken.block.SRLatchBlock;
-import dev.chyken.block.state.properties.SRLatchPart;
+import dev.chyken.block.state.properties.DoubleBlockPart;
 import dev.chyken.logicc.LogicCircuitsMod;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
@@ -11,11 +11,28 @@ import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
 import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 
 public class LogicBlockStateProvider extends BlockStateProvider {
 
     public LogicBlockStateProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
         super(output, LogicCircuitsMod.MODID, existingFileHelper);
+    }
+
+    @NotNull
+    private ConfiguredModel[] getConfiguredModels(Direction facing, ModelFile activeModel) {
+        int yRotation = switch (facing) {
+            case SOUTH -> 0;
+            case WEST -> 90;
+            case NORTH -> 180;
+            case EAST -> 270;
+            default -> 0;
+        };
+
+        return ConfiguredModel.builder()
+                .modelFile(activeModel)
+                .rotationY(yRotation)
+                .build();
     }
 
     private void registerLogicGate(net.minecraft.world.level.block.Block block, String baseName) {
@@ -28,18 +45,29 @@ public class LogicBlockStateProvider extends BlockStateProvider {
 
             ModelFile activeModel = powered ? gateOn : gateOff;
 
-            int yRotation = switch (facing) {
-                case SOUTH -> 0;
-                case WEST -> 90;
-                case NORTH -> 180;
-                case EAST -> 270;
-                default -> 0;
-            };
+            return getConfiguredModels(facing, activeModel);
+        });
+    }
 
-            return ConfiguredModel.builder()
-                    .modelFile(activeModel)
-                    .rotationY(yRotation)
-                    .build();
+    private void registerDouble(net.minecraft.world.level.block.Block block, String primaryName, String secondaryName) {
+        ModelFile primaryOff = models().getExistingFile(modLoc("block/" + primaryName));
+        ModelFile primaryOn = models().getExistingFile(modLoc("block/" + primaryName + "_on"));
+        ModelFile secondaryOff = models().getExistingFile(modLoc("block/" + secondaryName));
+        ModelFile secondaryOn = models().getExistingFile(modLoc("block/" + secondaryName + "_on"));
+
+        getVariantBuilder(block).forAllStates(state -> {
+            Direction facing = state.getValue(SRLatchBlock.FACING);
+            boolean powered = state.getValue(SRLatchBlock.POWERED);
+            var part = state.getValue(SRLatchBlock.PART);
+
+            ModelFile activeModel;
+            if (part == DoubleBlockPart.LEFT) {
+                activeModel = powered ? primaryOn : primaryOff;
+            } else {
+                activeModel = powered ? secondaryOn : secondaryOff;
+            }
+
+            return getConfiguredModels(facing, activeModel);
         });
     }
 
@@ -53,35 +81,6 @@ public class LogicBlockStateProvider extends BlockStateProvider {
         registerLogicGate(LogicBlocks.XOR_GATE.get(), "xor_gate");
         registerLogicGate(LogicBlocks.XNOR_GATE.get(), "xnor_gate");
 
-        ModelFile resetOff = models().getExistingFile(modLoc("block/reset"));
-        ModelFile resetOn = models().getExistingFile(modLoc("block/reset_on"));
-        ModelFile setOff = models().getExistingFile(modLoc("block/set"));
-        ModelFile setOn = models().getExistingFile(modLoc("block/set_on"));
-
-        getVariantBuilder(LogicBlocks.SR_LATCH.get()).forAllStates(state -> {
-            Direction facing = state.getValue(SRLatchBlock.FACING);
-            boolean powered = state.getValue(SRLatchBlock.POWERED);
-            var part = state.getValue(SRLatchBlock.PART);
-
-            ModelFile activeModel;
-            if (part == SRLatchPart.RESET) {
-                activeModel = powered ? resetOn : resetOff;
-            } else {
-                activeModel = powered ? setOn : setOff;
-            }
-
-            int yRotation = switch (facing) {
-                case SOUTH -> 0;
-                case WEST -> 90;
-                case NORTH -> 180;
-                case EAST -> 270;
-                default -> 0;
-            };
-
-            return ConfiguredModel.builder()
-                    .modelFile(activeModel)
-                    .rotationY(yRotation)
-                    .build();
-        });
+        registerDouble(LogicBlocks.SR_LATCH.get(), "reset", "set");
     }
 }
