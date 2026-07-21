c2fc4b3d4097099908b3ce392ccf91171b4f697d
chyken-nuggets <crusadersio.ezekiel06@gmail.com>
2026-07-21 22:38:29 +0800

    fix(loot): use createSinglePropConditionTable for multipart blocks

--- src/main/java/dev/chyken/data/LogicBlockLootSubProvider.java
+++ src/main/java/dev/chyken/data/LogicBlockLootSubProvider.java
@@ -1,6 +1,11 @@
 package dev.chyken.data;
 
 import dev.chyken.block.LogicBlocks;
+import dev.chyken.block.adders.FullAdderBlock;
+import dev.chyken.block.adders.HalfAdderBlock;
+import dev.chyken.block.latches.SRLatchBlock;
+import dev.chyken.block.state.properties.DoubleBlockPart;
+import dev.chyken.block.types.TwoPartLogicBlock;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
@@ -24,9 +29,30 @@ public class LogicBlockLootSubProvider extends BlockLootSubProvider {
         this.dropSelf(LogicBlocks.XOR_GATE.get());
         this.dropSelf(LogicBlocks.XNOR_GATE.get());
 
-        this.dropSelf(LogicBlocks.HALF_ADDER.get());
-        this.dropSelf(LogicBlocks.FULL_ADDER.get());
-        this.dropSelf(LogicBlocks.SR_LATCH.get());
+        this.add(
+                LogicBlocks.HALF_ADDER.get(),
+                block -> createSinglePropConditionTable(
+                        block,
+                        HalfAdderBlock.PART,
+                        DoubleBlockPart.RIGHT
+                )
+        );
+        this.add(
+                LogicBlocks.FULL_ADDER.get(),
+                block -> createSinglePropConditionTable(
+                        block,
+                        FullAdderBlock.PART,
+                        DoubleBlockPart.RIGHT
+                )
+        );
+        this.add(
+                LogicBlocks.SR_LATCH.get(),
+                block -> createSinglePropConditionTable(
+                        block,
+                        SRLatchBlock.PART,
+                        DoubleBlockPart.RIGHT
+                )
+        );
     }
 
     @Override
